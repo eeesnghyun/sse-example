0afd1bc5610f308d60391acb7c3ba81cce461761
eeesnghyun <szhyun2002@gmail.com>
2023-03-01 22:48:55 +0900

    [chore] eventSource를 통해 서버/클라이언트 연결

--- webflux-fe/src/App.js
+++ webflux-fe/src/App.js
@@ -3,14 +3,31 @@ import './App.css';
 function init() {
     const connectStore = () => {
       const storeCode = document.getElementById("storeCode").value;
-      alert(storeCode);
+      if (storeCode === "") return;
+      
+      const eventSource = new EventSource(`http://localhost:8080/notify/connect/${storeCode}`);
+    
+      eventSource.onopen = (e) => {
+          //console.log(e);
+      };	
+      eventSource.onerror = (e) => {
+          //console.log(e);
+      };	
+      eventSource.onmessage = (e) => {
+        console.log(e.data);
+      };
     };
 
     return (      
-    <div>      
-      <h2>가게 코드 : <input type='text' id='storeCode' placeholder='가게 코드를 입력해주세요.'/></h2>    
-      <button type="button" onClick={connectStore}>접속</button>    
-    </div>        
+    <div>   
+      <div>
+        <label>가게 코드 : </label><input type='text' id='storeCode' placeholder='가게 코드를 입력해주세요.'/>
+        <button type="button" onClick={connectStore}>접속</button>    
+      </div>   
+      <div>
+        <div id="messageArea"></div>
+      </div>
+    </div>            
   );
 }
 

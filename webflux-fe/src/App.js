1d7d53c8ff21ae3e9fe84d9955df1e99ca3e4a3c
eeesnghyun <szhyun2002@gmail.com>
2023-03-06 23:07:38 +0900

    [chore] 데이터 파싱 추가

--- webflux-fe/src/App.js
+++ webflux-fe/src/App.js
@@ -8,25 +8,31 @@ function init() {
       const eventSource = new EventSource(`http://localhost:8080/notify/connect/${storeCode}`);
     
       eventSource.onopen = (e) => {
-          //console.log(e);
+        // console.log(e);
       };	
       eventSource.onerror = (e) => {
-          //console.log(e);
+        // console.log(e);
       };	
       eventSource.onmessage = (e) => {
-        console.log(e.data);
+        // console.log(e.data);
+
+        if (e.data !== "tick") {
+          const result = JSON.parse(e.data);
+
+          document.getElementById("messageArea").innerHTML += `<p>! ${result.message} !</p>`;
+        }
       };
     };
 
-    return (      
+    return (
     <div>   
       <div>
         <label>가게 코드 : </label><input type='text' id='storeCode' placeholder='가게 코드를 입력해주세요.'/>
         <button type="button" onClick={connectStore}>접속</button>    
-      </div>   
-      <div>
-        <div id="messageArea"></div>
-      </div>
+      </div>      
+      <hr></hr>
+      <div>메세지</div>
+      <div id="messageArea"></div>
     </div>            
   );
 }
